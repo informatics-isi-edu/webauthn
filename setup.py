f02f9b683caa3201630864de58e6590545c0e7a7
bd4 <bd4@users.noreply.github.com>
2013-04-04 20:01:41 +0000

    add setup.py and install docs

---
+++ setup.py
@@ -0,0 +1,33 @@
+
+#
+# Copyright 2012 University of Southern California
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from distutils.core import setup
+
+setup(name="webauthn2",
+      description="web app security context management system",
+      version="0.1",
+      package_dir={"webauthn2": ""},
+      packages=["webauthn2", "webauthn2.providers"],
+      requires=["web.py", "pytz", "psycopg2", "oauth", "suds"],
+      classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+     )
