703ff4ae2acd20c4658bbf867a6cf22728cf46da
Karl Czajkowski <karlcz@isi.edu>
2016-04-01 17:21:09 -0700

    add isrd-systems to catalog ownership ACL by default during upgrade
    can be suppressed with ADD_ISRD_SYS=false env variable...

--- webauthn2/scripts/globus_auth_upgrade/ermrest-acl-etl.sh
+++ webauthn2/scripts/globus_auth_upgrade/ermrest-acl-etl.sh
@@ -4,6 +4,9 @@
 
 # set ADD_ISRD_STAFF=true to augment catalog ACLs w/ staff data read/write access
 ADD_ISRD_STAFF=${ADD_ISRD_STAFF:-false}
+# set ADD_ISRD_SYS=false to suppress augmenting catalog ACLs w/ systems ownership
+ADD_ISRD_SYS=${ADD_ISRD_SYS:-true}
+
 
 # for each registered catalog...
 su -c "psql -q -t -A -c \"select (descriptor::json)->>'dbname' from simple_registry\" ermrest" - ermrest \
@@ -16,6 +19,12 @@ do
     then
 	staff='https://auth.globus.org/176baec4-ed26-11e5-8e88-22000ab4b42b'
 	su -c "psql -q -t -A -c \"insert into _ermrest.meta (key, value) (values ('content_write_user', '$staff'), ('read_user', '$staff'), ('content_read_user', '$staff') except select key, value from _ermrest.meta) returning * \" $dbname" - ermrest
+    fi
+
+    if [[ "${ADD_ISRD_SYS}" = "true" ]]
+    then
+	systems='https://auth.globus.org/3938e0d0-ed35-11e5-8641-22000ab4b42b'
+	su -c "psql -q -t -A -c \"insert into _ermrest.meta (key, value) (values ('owner', '$systems') except select key, value from _ermrest.meta) returning * \" $dbname" - ermrest
     fi
 
 done
