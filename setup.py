732ca65c6b2ae9c7bdf60563a331aeb1ec0c7e34
Karl Czajkowski <karlcz@isi.edu>
2016-05-23 17:51:54 -0700

    wip: refactored standalone /authn API service
    A few targets for convenience in development and test:
    
    1.No more PLATFORM make variable
      - Portable runtime probes instead
      - Optional `preinstall_centos` and `preinstall_ubuntu` targets
    2. `install` target does `python ./setup.py install`
    3. `deploy` target does idempotent `webauthn2-deploy`
    
    1. `webauthn` UNIX daemon account
    2. `webauthn` PostgreSQL user role
    3. `webauthn` database
    4. `wsgi_webauthn2.conf` setup for Apache HTTPD w/ mod_wsgi
    5. Default deploy configures database providers for demo/test
    
    - Standalone test script
    - Working TravisCI config
    - Refactor Manager.get_request_context()
      - Current functionality for internal use in WSGI Daemon
      - Simpler shim for use from other applications like ERMrest
    - Migration plan for existing servers

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 
 #
-# Copyright 2012 University of Southern California
+# Copyright 2012-2015 University of Southern California
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,13 +16,30 @@
 #
 
 from distutils.core import setup
+import sys
+
+# this will be relative to sys.prefix e.g. /usr/share/webauthn2
+datadir = 'share/webauthn2'
 
 setup(
     name="webauthn2",
     description="web app security context management system",
     version="0.1-prerelease",
     packages=["webauthn2", "webauthn2.providers"],
-    requires=["web.py", "pytz", "psycopg2", "oauth", "suds"],
+    scripts=["bin/webauthn2-db-init", "bin/webauthn2-manage", "bin/webauthn2-deploy"],
+    package_data={
+        'webauthn2': ['webauthn2.wsgi'],
+    },
+    data_files=[
+        (datadir, [
+            "samples/webauthn2_config.json",
+            "samples/wsgi_webauthn2.conf",
+        ]),
+        (datadir + '/database', [
+            "samples/database/webauthn2_config.json",
+        ])
+    ],
+    requires=["web.py", "pytz", "psycopg2", "oauth", "oauth2client", "pyjwkest"],
     maintainer_email="support@misd.isi.edu",
     license='Apache License, Version 2.0',
     classifiers=[
